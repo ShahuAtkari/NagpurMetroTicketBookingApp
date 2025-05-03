8d304a91c8a426a53d26b43aa3b1ee5c7a186848
ShahuAtkari <shahu.atkari2116@gmail.com>
2025-05-03 10:58:39 +0530

    adding Metro Project.

---
+++ src/main/java/com/NagpurMetro/Binding/MetroTicketBook.java
@@ -0,0 +1,11 @@
+package com.NagpurMetro.Binding;
+
+import lombok.Data;
+
+@Data
+public class MetroTicketBook {
+	
+	private String boardingStation;
+	private String destinationStation;
+	private Integer numberofPassenger;		
+}
